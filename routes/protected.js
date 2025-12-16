784cf58dbefb784e7522a0c4e388bd82617af7a7
FinnakaLite <Finng2008@outlook.com>
2025-12-16 13:28:59 +0100

    Protected.js

---
+++ routes/protected.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db");
+const requiresLogin = require("../auth.js");
+const bcrypt = require("bcrypt");
+
+router.get("/", requiresLogin, (req, res) => {
+    res.send(`<p>Welcome to a protected page, ${req.session.firstName}!
+        <button onclick></button>`)
+})
