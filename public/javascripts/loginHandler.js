fd1487c5b921be33a65bb482e27c692a04210dca
FinnakaLite <Finng2008@outlook.com>
2025-12-16 13:22:54 +0100

    Updated Handler

--- public/javascripts/loginHandler.js
+++ public/javascripts/loginHandler.js
@@ -1,6 +1,21 @@
-async function loggInn(event) {
+async function loggIn(event) {
     event.preventDefault();
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
-    const response = await
+
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({email, password})
+    });
+
+    const result = await response.json();
+    if (result.status === 200) {
+        alert(result.message);
+        window.location.href = "/beskyttet";
+    } else {
+        alert(result.message);
+    }
 }
